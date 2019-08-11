62dc81cfc25bf216de573979ffd1e87e97311162
RadoslawSzymansky <radek199@gmail.com>
2019-08-11 20:13:31 +0200

    register user witkour send jwt yet<::

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('hello world') )
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 // to na heroku poszuka portu gdy bedzie odpalac node PORT=50010 np a jak nie to 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
 
+  
