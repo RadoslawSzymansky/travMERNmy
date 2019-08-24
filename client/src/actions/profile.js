1076158208926b08c0f25f6f2a0ed0f3093f59c7
RadoslawSzymansky <radek199@gmail.com>
2019-08-24 10:16:51 +0200

    vid 57 6min

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,9 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   ACCOUNT_DELETED,
-  CLEAR_PROFILE
+  CLEAR_PROFILE,
+  GET_REPOS,
+  GET_PROFILES
 } from './types';
 
 // Get current users profile
@@ -15,13 +17,76 @@ export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('/api/profile/me');
     dispatch({
-      type: GET_PROFILE,
+      type: GET_PROFILES,
       payload: res.data
     })
   } catch (error) {
     dispatch({
       type: PROFILE_ERROR,
       payload: { 
+        msg: error.response.statusText,
+        status: error.response.status
+      }
+    });
+  };
+};
+
+
+// Get All profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    })
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status
+      }
+    });
+  };
+};
+
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status
+      }
+    });
+  };
+};
+
+// Get github repos
+export const getGithubRepos = username => async dispatch => {
+
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
         msg: error.response.statusText,
         status: error.response.status
       }
@@ -185,7 +250,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if(window.confirm('Are you sure? This can NOT be undone!')){
     try {
-      const res = await axios.delete('/api/profile');
+      await axios.delete('/api/profile');
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
 
@@ -201,3 +266,5 @@ export const deleteAccount = () => async dispatch => {
     };
   };
 };
+
+//
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -39,7 +39,7 @@ const EditProfile = ({ createProfile, history, getCurrentProfile, profile: { pro
       youtube: loading || !profile.youtube ? '' : profile.youtube,
       instagram: loading || !profile.instagram ? '' : profile.instagram
     })
-  }, [loading]);
+  }, [loading, getCurrentProfile]);
 
   const {
     company,
