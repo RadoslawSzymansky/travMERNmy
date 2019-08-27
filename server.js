932303509731e5eee179e8e2de067ff22e5bc85f
RadoslawSzymansky <radek199@gmail.com>
2019-08-27 19:39:24 +0200

    correct app.get nn serve

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ if(process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }));
+  });
 };
 
 // to na heroku poszuka portu gdy bedzie odpalac node PORT=50010 np a jak nie to 5000
