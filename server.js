2ba9133b0f9fe3f1df8df4f5e3b58e70fc0c6542
RadoslawSzymansky <radek199@gmail.com>
2019-08-27 19:35:17 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,24 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('hello world') )
+
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Server static assets in production
+
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  }));
+};
 
 // to na heroku poszuka portu gdy bedzie odpalac node PORT=50010 np a jak nie to 5000
 const PORT = process.env.PORT || 5000;
